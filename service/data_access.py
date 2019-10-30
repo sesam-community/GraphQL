ed62b1c70adde54f5696f85dc960ae05e49c8aee
palandreassen <pal.a@sesam.io>
2019-10-30 14:12:59 +0100

    Cleaned up code

--- service/data_access.py
+++ service/data_access.py
@@ -1,10 +1,7 @@
 import requests
 import logging
-from sesamutils import sesam_logger
-import json
 
-# logger = logging.getLogger(f"graphql.{__name__}")
-logger = sesam_logger("GraphQL")
+logger = logging.getLogger(f"GraphQL.{__name__}")
 
 
 class DataAccess:
@@ -15,17 +12,14 @@ class DataAccess:
         self.config = config
 
     def __get_all_entities(self, url, query):
-        logger.info('Fetching entities')
         url = self.config.baseurl + url
-        logger.debug("url: " + url)
+        logger.debug("full url: " + url)
         req = self.request("POST", url, query)
         if not req.ok:
-            logger.debug(req)
-            logger.info('request not ok')
+            logger.info('Request not ok')
         else:
-            logger.debug("req.ok in __get_all_entities")
             res = req.json()
-
+            logger.debug("request ok. yielding result.")
             yield res
 
     def get_entities(self, url, query):
@@ -59,29 +53,28 @@ class DataAccess:
         if "headers" in kwargs:
             headers = {**kwargs["headers"], **self.auth_header}
             kwargs.pop("headers")
-            logger.debug("headers in kwargs")
+            logger.debug("found headers in kwargs")
         else:
-            logger.debug("headers not in kwargs")
+            logger.debug("no headers in kwargs")
             headers = self.auth_header
 
         if "json" in kwargs:
             headers = {**headers, "Content-Type": "application/json"}
-            logger.debug("json in kwargs")
+            logger.debug("found json in kwargs")
 
         logger.debug("headers before req build: " + str(headers))
         logger.debug("query before req build: " + str(query))
 
         req = requests.Request(method, url, headers=headers, data=query, **kwargs)
-        logger.debug("got past requests.Request")
+        logger.debug("managed to build request")
         try:
             resp = self.session.send(req.prepare())
         except Exception as e:
             logger.error(f"Failed to send request. Error: {e}")
             raise
-        logger.debug("got past session.send(req.prepare)")
+
         if resp.status_code == 401:
             logger.warning("Received status 401. Requesting new access token.")
             self.get_token()
             resp = self.session.send(req.prepare())
-        logger.debug("got to end of request method")
         return resp
